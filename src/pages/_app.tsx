7859bc0a80ed6d31b38c3d4fa1c0777371e74a38
Hugo Ferreira <hugoferreirait@gmail.com>
2022-05-15 21:33:54 -0300

    chore: base layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>MUI CRUD</title>
+        <meta name="description" content="CRUD made with Material UI" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
