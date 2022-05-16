0cdf4ef02ddc899d51766ebe985a74e9fa123b82
Hugo Ferreira <hugoferreirait@gmail.com>
2022-05-15 23:03:11 -0300

    feat: add task

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,25 @@ import type { NextPage } from 'next'
 import { useState } from 'react';
 import DeleteIcon from '@mui/icons-material/Delete';
 import styles from '../styles/Home.module.css'
+import { Task } from 'typings/tasks';
+import { useTasks } from 'store';
 
 const Home: NextPage = () => {
-
+  const { tasks, setTasks } = useTasks();
   const [openCreateModal, setOpenCreateModal] = useState(false);
+  const [taskInfo, setTaskInfo] = useState<Task>({
+    name: '',
+    description: '',
+  })
+
+  const createTask = () => {
+    setTasks([...tasks, { ...taskInfo }]);
+    setTaskInfo({
+      name: '',
+      description: '',
+    })
+    setOpenCreateModal(false);
+  };
 
   return (
     <main className={styles.main}>
@@ -33,24 +48,47 @@ const Home: NextPage = () => {
             fontSize: 36,
           }}>+</Button>
         </div>
-        <Button variant="outlined" color="error" onClick={() => setOpenCreateModal(true)} startIcon={<DeleteIcon />}>
+
+        <div className={styles.tasksList}>
+          {tasks.map((t, index) => (
+            <div key={index}>
+              {t.name}
+            </div>
+          ))}
+        </div>
+
+
+        {/* <Button variant="outlined" color="error" onClick={() => setOpenCreateModal(true)} startIcon={<DeleteIcon />}>
           Delete
-        </Button>
+        </Button> */}
       </div>
 
       {openCreateModal && (
         <div className={styles.createModalBg}>
           <div className={styles.createModal}>
             <div className={styles.modalContent}>
-              <TextField variant="outlined" label="Name" sx={{
-                width: '100%',
-                zIndex: 0,
-                marginBottom: '20px',
-              }} />
-              <TextField multiline maxRows={8} minRows={8} variant="outlined" label="Description" sx={{
-                width: '100%',
-                zIndex: 0,
-              }} />
+              <TextField
+                variant="outlined"
+                label="Name"
+                value={taskInfo.name}
+                onChange={(e) => setTaskInfo({ ...taskInfo, name: e.target.value })}
+                sx={{
+                  width: '100%',
+                  zIndex: 0,
+                  marginBottom: '20px',
+                }} />
+              <TextField
+                multiline
+                maxRows={8}
+                minRows={8}
+                variant="outlined"
+                label="Description"
+                value={taskInfo.description}
+                onChange={(e) => setTaskInfo({ ...taskInfo, description: e.target.value })}
+                sx={{
+                  width: '100%',
+                  zIndex: 0,
+                }} />
             </div>
             <div className={styles.modalFooter}>
               <Button variant="outlined" color="error" onClick={() => setOpenCreateModal(false)} sx={{
@@ -58,10 +96,13 @@ const Home: NextPage = () => {
               }}>
                 Cancel
               </Button>
-              <Button variant="contained" onClick={() => setOpenCreateModal(false)} sx={{
-                height: 40,
-                marginLeft: '15px',
-              }}>
+              <Button
+                variant="contained"
+                onClick={createTask}
+                sx={{
+                  height: 40,
+                  marginLeft: '15px',
+                }}>
                 Create
               </Button>
             </div>
--- src/typings/tasks.ts
+++ src/typings/tasks.ts
@@ -1,5 +1,4 @@
 export interface Task {
-  id: number,
   name: string,
   description: string,
 }
